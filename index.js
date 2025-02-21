6fb382f8036c1593b6918eb59200797588c8e27e
matheus <matheusgois.pessoal@gmail.com>
2025-02-21 17:29:15 -0300

    deu bom o postman

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const user_route = require('./routes/user')
 
 dotenv.config()
 
@@ -9,9 +10,8 @@ const app = express()
 mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB connection successfull!")).catch((err)=>{console.log(err)})
 
 // routes
-app.get("/api/test", () => {
-    console.log("test is successfull")
-})
+app.use(express.json())
+app.use('/api/users', user_route)
 
 app.listen(process.env.PORT, () => {
     console.log("Running on 5000 port")
